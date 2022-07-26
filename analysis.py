857cd8a91234c050dffaa1b155769f23ac77667b
aemiliaurban <urban.nicole.d@gmail.com>
2022-07-27 00:08:36 +0200

    working version in python3

--- analysis.py
+++ analysis.py
@@ -6,99 +6,138 @@ import numpy
 from imagen import SineGrating
 import pylab
 
-def fullfieldSineGratingOrientationTuningProtocol(model,retina,sheets=None,num_orientation=8,num_phase=10,duration=0.04,frequency=2.4,scale=1.0,filename=None,plot=False,load=False,reset=False):
+
+def fullfieldSineGratingOrientationTuningProtocol(
+    model,
+    retina,
+    sheets=None,
+    num_orientation=8,
+    num_phase=10,
+    duration=0.04,
+    frequency=2.4,
+    scale=1.0,
+    filename=None,
+    plot=False,
+    load=False,
+    reset=False,
+):
     """
     This analysis will present fullfield sine grating orientation tuning protocol to the model *model*.
-    
+
     It will collect the final activation of neurons in sheets *sheets* (if None all sheets in the model are analyzed) and compute orientation tuning preference and
     selectivity for the neurons in these sheets.
-    
+
     If filename is not None it will save the collected data to file, filename
     """
     if not load:
         responses = {}
-        
+
         if sheets == None:
-           sheets = model.sheets
-        
-        #initialize the 
+            sheets = model.sheets
+
+        # initialize the
         for s in sheets:
-            responses[s.name] = numpy.zeros((num_orientation,num_phase,(s.unit_diameter)**2))
-        
-        #present the stimulation protocol and collect data
-        for i in xrange(num_orientation):
-            for j in xrange(num_phase):
+            responses[s.name] = numpy.zeros(
+                (num_orientation, num_phase, (s.unit_diameter) ** 2)
+            )
+
+        # present the stimulation protocol and collect data
+        for i in range(num_orientation):
+            for j in range(num_phase):
                 if reset:
                     for s in model.sheets:
                         s.reset()
 
-                stim = SineGrating(orientation=numpy.pi/num_orientation*i,phase=numpy.pi*2/num_phase*j,xdensity=retina.unit_diameter,ydensity=retina.unit_diameter,frequency=frequency,scale=scale)()
+                stim = SineGrating(
+                    orientation=numpy.pi / num_orientation * i,
+                    phase=numpy.pi * 2 / num_phase * j,
+                    xdensity=retina.unit_diameter,
+                    ydensity=retina.unit_diameter,
+                    frequency=frequency,
+                    scale=scale,
+                )()
                 retina.set_activity(stim)
                 model.run(duration)
                 for s in sheets:
-                    responses[s.name][i,j,:] = s.get_activity(model.dt).copy().ravel()    
-                    
+                    responses[s.name][i, j, :] = s.get_activity(model.dt).copy().ravel()
+
         if filename != None:
-           import pickle
-           f = open(filename+"_resp.pickle",'wb')
-           pickle.dump(responses,f)
-           f.close()
+            import pickle
+
+            f = open(filename + "_resp.pickle", "wb")
+            pickle.dump(responses, f)
+            f.close()
     else:
-           import pickle
-           f = open(filename+"_resp.pickle",'rb')
-           responses = pickle.load(f)
-           f.close()
-    
+        import pickle
+
+        f = open(filename + "_resp.pickle", "rb")
+        responses = pickle.load(f)
+        f.close()
+
     # lets calculate the orientation preference and selectivity
-    angles = [2*numpy.pi/num_orientation*i for i in xrange(num_orientation)] 
+    angles = [2 * numpy.pi / num_orientation * i for i in range(num_orientation)]
     angles_as_complex_numbers = numpy.cos(angles) + numpy.sin(angles) * 1j
-    
+
     orientation_preference_maps = {}
     orientation_selectivity_maps = {}
-    
+
     pylab.figure()
     for k in responses.keys():
         # first let's select the response to each orientation as the maximum response across phases
-        resp = numpy.max(responses[k],axis=1)
-        
-        
-        
+        resp = numpy.max(responses[k], axis=1)
+
         sheet_size = numpy.sqrt(len(resp[0]))
         for i in range(len(angles)):
-            pylab.subplot(1,len(angles),i) 
-            
-            pylab.imshow(numpy.reshape(resp[i],(sheet_size,sheet_size)),interpolation='none',cmap='gray')
-            pylab.colorbar()
-            
+            if i > 0:
+                pylab.subplot(1, len(angles), i)
+
+                pylab.imshow(
+                    numpy.reshape(resp[i], (int(sheet_size), int(sheet_size))),
+                    interpolation="none",
+                    cmap="gray",
+                )
+                pylab.colorbar()
+
         # calculate selectivity and preference as the angle and magnitude of the mean of the responses projected into polar coordinates based on the orientation angle
         # to which they were collected
-        polar_resp = resp * numpy.array(angles_as_complex_numbers)[:,numpy.newaxis]
-        polar_mean_resp = numpy.mean(polar_resp,axis=0)
-        
-        orientation_preference_maps[k] = (numpy.angle(polar_mean_resp) + 4*numpy.pi) % (numpy.pi*2)
+        polar_resp = resp * numpy.array(angles_as_complex_numbers)[:, numpy.newaxis]
+        polar_mean_resp = numpy.mean(polar_resp, axis=0)
+
+        orientation_preference_maps[k] = (
+            numpy.angle(polar_mean_resp) + 4 * numpy.pi
+        ) % (numpy.pi * 2)
         orientation_selectivity_maps[k] = numpy.absolute(polar_mean_resp)
-    
-    pylab.savefig(filename+"_or_resps.png",dpi=200)
-    
+
+    pylab.savefig(filename + "_or_resps.png", dpi=200)
+
     pylab.figure()
     # make a plot of the maps if asked for
     if plot:
-       gs = gridspec.GridSpec(2, len(responses.keys()))
-       gs.update(left=0.05, right=0.95, top=0.95, bottom=0.05,hspace=0.1,wspace=0.1)
-       
-       for i,s in enumerate(responses.keys()):
-           sheet_size = numpy.sqrt(len(orientation_preference_maps[k]))
-           pylab.subplot(gs[0,i])
-           im=pylab.imshow(numpy.resize(orientation_preference_maps[k],(sheet_size,sheet_size)),vmin=0,vmax=2*numpy.pi,interpolation='nearest',cmap='hsv')
-           pylab.axis('off')    
-           pylab.colorbar(im,fraction=0.046, pad=0.04)
-           
-           pylab.subplot(gs[1,i])
-           im=pylab.imshow(numpy.resize(orientation_selectivity_maps[k],(sheet_size,sheet_size)),interpolation='nearest',cmap='gray')
-           pylab.axis('off')
-           pylab.colorbar(im,fraction=0.046, pad=0.04)
-        
+        gs = gridspec.GridSpec(2, len(responses.keys()))
+        gs.update(left=0.05, right=0.95, top=0.95, bottom=0.05, hspace=0.1, wspace=0.1)
 
-       pylab.savefig(filename+"_maps.png",dpi=200)
-    
-    return orientation_preference_maps,orientation_selectivity_maps
+        for i, s in enumerate(responses.keys()):
+            sheet_size = numpy.sqrt(len(orientation_preference_maps[k]))
+            pylab.subplot(gs[0, i])
+            im = pylab.imshow(
+                numpy.resize(orientation_preference_maps[k], (int(sheet_size), int(sheet_size))),
+                vmin=0,
+                vmax=2 * numpy.pi,
+                interpolation="nearest",
+                cmap="hsv",
+            )
+            pylab.axis("off")
+            pylab.colorbar(im, fraction=0.046, pad=0.04)
+
+            pylab.subplot(gs[1, i])
+            im = pylab.imshow(
+                numpy.resize(orientation_selectivity_maps[k], (int(sheet_size), int(sheet_size))),
+                interpolation="nearest",
+                cmap="gray",
+            )
+            pylab.axis("off")
+            pylab.colorbar(im, fraction=0.046, pad=0.04)
+
+        pylab.savefig(filename + "_maps.png", dpi=200)
+
+    return orientation_preference_maps, orientation_selectivity_maps
--- projections.py
+++ projections.py
@@ -1,18 +1,19 @@
 import numpy
 import scipy
-from imagen.boundingregion import BoundingBox
+
 import imagen
+from holoviews import BoundingBox
 
 
-class Projection(object):    
+class Projection(object):
     """
     A set of connections from one sheet to another. This is an abstract class.
     """
-    
-    def __init__(self, name, source, target, strength,delay):
+
+    def __init__(self, name, source, target, strength, delay):
         """
         Each Projection has a source sheet and target sheet.
-        
+
         Parameters
         ----------
         source : Sheet
@@ -20,10 +21,10 @@ class Projection(object):
 
         target : Sheet
                  The sheet to which projection passes rates.
-                 
+
         strength : float
                 The strength of the projection.
-        
+
         delay : float
                 The delay of the projection
         """
@@ -36,131 +37,200 @@ class Projection(object):
         self.target._register_in_projection(self)
         self.source._register_out_projection(self)
         self.activity = None
-        self.changed =True
-        
-    
+        self.changed = True
+
     def activate(self):
         """
         This returns a matrix of the same size as the target sheet, which corresponds to the contributions from this projections to the individual neurons.
         """
-        raise NotImplementedError   
+        raise NotImplementedError
 
 
-class FastConnetcionFieldProjection(Projection):    
+class FastConnetcionFieldProjection(Projection):
     """
     A projection which has can have different connection field for each target neuron, which are assumed to be centered on the target's neuron position.
     This means that the output of ConvolutionalProjection is the spatial convolution of this connection kernel with the activities of the source Sheet.
-    
-    Note that all connection fields, including the border ones, will be normalized so that the sum of their absolute values is 1.    
+
+    Note that all connection fields, including the border ones, will be normalized so that the sum of their absolute values is 1.
     """
-    def __init__(self,name,source,target,strength,delay,radius,initial_connection_kernel):
+
+    def __init__(
+        self, name, source, target, strength, delay, radius, initial_connection_kernel
+    ):
         """
         Each Projection has a source sheet and target sheet.
-        
+
         Parameters
         ----------
         radius : float
-                 How big the connections fields will be in the source sheet coordinates!   
+                 How big the connections fields will be in the source sheet coordinates!
         source : Sheet
                  The sheet from which projection passes rates.
 
         target : Sheet
                  The sheet to which projection passes rates.
-                 
+
         strength : float
                  The strength of the projection.
-                 
+
         inititial_connection_kernel: ndarray
-                 The initial connection kernel. 
+                 The initial connection kernel.
         """
-        Projection.__init__(self, name,source, target, strength,delay)
+        Projection.__init__(self, name, source, target, strength, delay)
         self.radius = radius
-        
-        mask = imagen.Disk(xdensity = source.density,ydensity = source.density,bounds = BoundingBox(radius=source.size/2),size=self.radius*2,smoothing=0)
+
+        mask = imagen.Disk(
+            xdensity=source.density,
+            ydensity=source.density,
+            bounds=BoundingBox(radius=source.size / 2),
+            size=self.radius * 2,
+            smoothing=0,
+        )
         initial_connection_kernel.xdensity = source.density
         initial_connection_kernel.ydensity = source.density
-        initial_connection_kernel.bounds = BoundingBox(radius=source.size/2.0)
-        
+        initial_connection_kernel.bounds = BoundingBox(radius=source.size / 2.0)
+
         source_dim = self.source.unit_diameter
         target_dim = self.target.unit_diameter
-        self.cfs = numpy.zeros((target_dim*target_dim,source_dim*source_dim),dtype=numpy.float32)
-        self.masks = numpy.zeros((target_dim*target_dim,source_dim*source_dim),dtype=numpy.float32)
-        
-        for i in xrange(target_dim):
-            for j in xrange(target_dim):
-                ci,cj = self.target.index_to_coord(i,j)
-                self.cfs[i*target_dim+j,:] = initial_connection_kernel(x=cj,y=-ci).flatten()
-                self.masks[i*target_dim+j,:] = mask(x=cj,y=-ci).flatten()
-        
-        #apply masks and normalize
-        self.cfs = numpy.multiply(self.cfs,self.masks)
-        self.cfs = self.cfs / numpy.sum(numpy.abs(self.cfs),axis=1)[:,numpy.newaxis]
-  
+        self.cfs = numpy.zeros(
+            (target_dim * target_dim, source_dim * source_dim), dtype=numpy.float32
+        )
+        self.masks = numpy.zeros(
+            (target_dim * target_dim, source_dim * source_dim), dtype=numpy.float32
+        )
+
+        for i in range(target_dim):
+            for j in range(target_dim):
+                ci, cj = self.target.index_to_coord(i, j)
+                self.cfs[i * target_dim + j, :] = initial_connection_kernel(
+                    x=cj, y=-ci
+                ).flatten()
+                self.masks[i * target_dim + j, :] = mask(x=cj, y=-ci).flatten()
+
+        # apply masks and normalize
+        self.cfs = numpy.multiply(self.cfs, self.masks)
+        self.cfs = self.cfs / numpy.sum(numpy.abs(self.cfs), axis=1)[:, numpy.newaxis]
+
         assert numpy.min(self.masks) == 0 and numpy.max(self.masks) == 1
-        self.activity = numpy.zeros((target_dim,target_dim),dtype=numpy.float32)
-    
+        self.activity = numpy.zeros((target_dim, target_dim), dtype=numpy.float32)
+
     def activate(self):
         """
         This returns a matrix of the same size as the target sheet, which corresponds to the contributions from this projections to the individual neurons.
         """
         sa = self.source.get_activity(self.delay).ravel()
-        self.activity = self.strength * numpy.reshape(numpy.dot(self.cfs,sa),(self.target.unit_diameter,self.target.unit_diameter))
-        
-    def get_cf(self,posx,posy):
-        ci,cj = self.target.index_to_coord(posx,posy)
-        xi,yi = self.source.coord_to_index(ci-self.radius,cj-self.radius,clipped=True)
-        xa,ya = self.source.coord_to_index(ci+self.radius,cj+self.radius,clipped=True)
-        return numpy.reshape(self.cfs[posx*self.target.unit_diameter+posy,:],(self.source.unit_diameter,self.source.unit_diameter))[xi:xa,yi:ya]
-        
-    def applyHebianLearningStep(self,learning_rate):
+        self.activity = self.strength * numpy.reshape(
+            numpy.dot(self.cfs, sa),
+            (self.target.unit_diameter, self.target.unit_diameter),
+        )
+
+    def get_cf(self, posx, posy):
+        ci, cj = self.target.index_to_coord(posx, posy)
+        xi, yi = self.source.coord_to_index(
+            ci - self.radius, cj - self.radius, clipped=True
+        )
+        xa, ya = self.source.coord_to_index(
+            ci + self.radius, cj + self.radius, clipped=True
+        )
+        xi, yi, xa, ya = int(xi), int(yi), int(xa), int(ya)
+        return numpy.reshape(
+            self.cfs[posx * self.target.unit_diameter + posy, :],
+            (self.source.unit_diameter, self.source.unit_diameter),
+        )[xi:xa, yi:ya]
+
+    def applyHebianLearningStep(self, learning_rate):
         """
         This method when applied to a ConnetcionFieldProjection will perform a single step of hebbian learning with learning rate *learning_rate*.
         """
         sa = self.source.get_activity(self.delay).ravel()
         ta = self.target.get_activity(self.target.dt).ravel()
-        self.cfs += learning_rate * numpy.dot(ta[:,numpy.newaxis],sa[numpy.newaxis,:])
-        self.cfs = numpy.multiply(self.cfs,self.masks)
-        self.cfs = self.cfs / numpy.sum(numpy.abs(self.cfs),axis=1)[:,numpy.newaxis]
+        self.cfs += learning_rate * numpy.dot(
+            ta[:, numpy.newaxis], sa[numpy.newaxis, :]
+        )
+        self.cfs = numpy.multiply(self.cfs, self.masks)
+        self.cfs = self.cfs / numpy.sum(numpy.abs(self.cfs), axis=1)[:, numpy.newaxis]
 
-class ConvolutionalProjection(Projection):    
+
+class ConvolutionalProjection(Projection):
     """
     A projection which has only one set of connections (connection kernel) which are assumed to be the same for all target neurons, except being centered on their position.
     This means that the output of ConvolutionalProjection is the spatial convolution of this connection kernel with the activities of the source Sheet.
-    
+
     Note that the connection_kernel will be normalized so that the sum of it's absolute values is 1.
-    
+
     ConvolutionalProjection is only allowed between sheets of the same size and density.
     """
-    def __init__(self,name,source,target,strength,delay,connection_kernel):
+
+    def __init__(self, name, source, target, strength, delay, connection_kernel):
         """
         Each Projection has a source sheet and target sheet.
-        
+
         Parameters
         ----------
         source : Sheet
                  The sheet from which projection passes rates.
         target : Sheet
                  The sheet to which projection passes rates.
-                 
+
         strength : float
                  The strength of the projection.
         """
-        Projection.__init__(self, name,source, target, strength,delay)
-        self.connection_kernel = connection_kernel #/ numpy.sum(numpy.abs(connection_kernel))
-        assert self.source.size == self.target.size , "ERROR: The size of source sheet of ConvolutionalProjection has to be same as that of target sheet"
-        assert self.source.density == self.target.density , "ERROR: The density of source sheet of ConvolutionalProjection has to be same as that of target sheet"
+        Projection.__init__(self, name, source, target, strength, delay)
+        self.connection_kernel = (
+            connection_kernel  # / numpy.sum(numpy.abs(connection_kernel))
+        )
+        assert (
+            self.source.size == self.target.size
+        ), "ERROR: The size of source sheet of ConvolutionalProjection has to be same as that of target sheet"
+        assert (
+            self.source.density == self.target.density
+        ), "ERROR: The density of source sheet of ConvolutionalProjection has to be same as that of target sheet"
         assert self.target.unit_diameter == self.source.unit_diameter
-        assert numpy.shape(connection_kernel)[0] % 2 == 1, "ERROR: initial kernel for ConvolutionalProjection has to have odd radius"
-        #lets calculate edge correction factors:
-        self.rad = int((numpy.shape(connection_kernel)[0]-1)/2)
+        assert (
+            numpy.shape(connection_kernel)[0] % 2 == 1
+        ), "ERROR: initial kernel for ConvolutionalProjection has to have odd radius"
+        # lets calculate edge correction factors:
+        self.rad = int((numpy.shape(connection_kernel)[0] - 1) / 2)
         dim = self.target.unit_diameter
-        cfs = [[self.connection_kernel[max(0,self.rad-j):2*self.rad+1-max(0,(j+1+self.rad)-dim),max(0,self.rad-i):2*self.rad+1-max(0,(i+1+self.rad)-dim)] for i in xrange(dim)] for j in xrange(dim)]
-        self.corr_factors = numpy.array([[numpy.sum(numpy.abs(connection_kernel))/numpy.sum(numpy.abs(b)) for b in a] for a in cfs])
-        
+        cfs = [
+            [
+                self.connection_kernel[
+                    max(0, self.rad - j) : 2 * self.rad
+                    + 1
+                    - max(0, (j + 1 + self.rad) - dim),
+                    max(0, self.rad - i) : 2 * self.rad
+                    + 1
+                    - max(0, (i + 1 + self.rad) - dim),
+                ]
+                for i in range(dim)
+            ]
+            for j in range(dim)
+        ]
+        self.corr_factors = numpy.array(
+            [
+                [
+                    numpy.sum(numpy.abs(connection_kernel)) / numpy.sum(numpy.abs(b))
+                    for b in a
+                ]
+                for a in cfs
+            ]
+        )
+
     def activate(self):
         """
         This returns a matrix of the same size as the target sheet, which corresponds to the contributions from this projections to the individual neurons.
         """
-        resp = scipy.signal.fftconvolve(self.source.get_activity(self.delay),self.connection_kernel, mode='same')
-        assert numpy.shape(resp) == (self.target.unit_diameter,self.target.unit_diameter), "ERROR: The size of calculated projection respone is " + str(numpy.shape(resp)) + "units, while the size of target sheet is " + str((self.target.unit_diameter,self.target.unit_diameter)) + " units"
-        self.activity = numpy.multiply(resp,self.corr_factors) * self.strength
+        resp = scipy.signal.fftconvolve(
+            self.source.get_activity(self.delay), self.connection_kernel, mode="same"
+        )
+        assert numpy.shape(resp) == (
+            self.target.unit_diameter,
+            self.target.unit_diameter,
+        ), (
+            "ERROR: The size of calculated projection respone is "
+            + str(numpy.shape(resp))
+            + "units, while the size of target sheet is "
+            + str((self.target.unit_diameter, self.target.unit_diameter))
+            + " units"
+        )
+        self.activity = numpy.multiply(resp, self.corr_factors) * self.strength
